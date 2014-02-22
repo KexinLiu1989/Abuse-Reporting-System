7ebbbbc88949f5838410a8810ec8a97e9c67da73
Hao Zhou <hzhou@wpi.edu>
2014-02-22 13:01:14 -0500

    Add UserInterceptor Demo (AOP)

--- src/dds/controller/UserController.java
+++ src/dds/controller/UserController.java
@@ -1,14 +1,17 @@
 package dds.controller;
 
+
+import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
 
 import dds.common.User;
+import dds.interceptor.UserInterceptor;
 
 /**
  * UserController
  * Note: Real SQL Queries should be in Models, currently, I just do it for test
  */
-
+@Before(UserInterceptor.class)
 public class UserController extends Controller {
 	public void index() {
 		setAttr("users", User.dao.paginate(getParaToInt(0, 1), 10, "select *", "from user"));
