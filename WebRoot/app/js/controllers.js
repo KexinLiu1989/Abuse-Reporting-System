b46fa899c81ecb057624e176a4931eec2ddb6593
Cheng Deng <cdeng@fwdco.com>
2014-02-26 11:15:01 -0500

    added sample of cosuming restful using angular

--- WebRoot/app/js/controllers.js
+++ WebRoot/app/js/controllers.js
@@ -8,19 +8,19 @@ define(['angular', 'services'], function (angular) {
             // so that we could set "active" class based on current $location.path
             $scope.location = $location;
         }])
-		.controller('view1Ctrl', ['$scope', function ($scope) {
+		.controller('view1Ctrl', ['$scope', '$http', function ($scope, $http) {
             require(['controllers/view1Ctrl'], function(ctrl) {
                 // injector method takes an array of modules as the first argument
                 // if you want your controller to be able to use components from
                 // any of your other modules, make sure you include it together with 'ng'
                 // Furthermore we need to pass on the $scope as it's unique to this controller
-                angular.injector(['ng']).invoke(ctrl, this, { '$scope': $scope });
+                angular.injector(['ng']).invoke(ctrl, this, { '$scope': $scope, '$http': $http });
             });
 		}])
 		// More involved example where controller is required from an external file
-		.controller('view2Ctrl', ['$scope', function($scope) {
+		.controller('view2Ctrl', ['$scope', '$http', function($scope, $http) {
 			require(['controllers/view2Ctrl'], function(ctrl) {
-                angular.injector(['ng']).invoke(ctrl, this, { '$scope': $scope });
+                angular.injector(['ng']).invoke(ctrl, this, { '$scope': $scope, '$http': $http });
 			});
 		}]);
 });
--- WebRoot/app/js/controllers/view2Ctrl.js
+++ WebRoot/app/js/controllers/view2Ctrl.js
@@ -1,34 +1,16 @@
 define([], function() {
-	return ['$scope', function($scope) {
-		// You can access the scope of the controller from here
-        $scope.todos = [
-            {text:'learn angular', done:true},
-            {text:'build an angular app', done:false}];
+	return ['$scope', '$http', function($scope, $http) {
 
-        $scope.addTodo = function() {
-            $scope.todos.push({text:$scope.todoText, done:false});
-            $scope.todoText = '';
+        // get user data from /user method
+        $scope.getUser = function () {
+            $http.get('user').
+                then(function (response) {
+                    var data = response.data;
+                    // put data into scope
+                    $scope.userData = data.users.list;
+                });
         };
+        $scope.getUser();
 
-        $scope.remaining = function() {
-            var count = 0;
-            angular.forEach($scope.todos, function(todo) {
-                count += todo.done ? 0 : 1;
-            });
-            return count;
-        };
-
-        $scope.archive = function() {
-            var oldTodos = $scope.todos;
-            $scope.todos = [];
-            angular.forEach(oldTodos, function(todo) {
-                if (!todo.done) $scope.todos.push(todo);
-            });
-        };
-
-		// because this has happened asynchroneusly we've missed
-		// Angular's initial call to $apply after the controller has been loaded
-		// hence we need to explicityly call it at the end of our Controller constructor
-		$scope.$apply();
 	}];
 });
