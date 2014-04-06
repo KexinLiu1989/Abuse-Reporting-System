e6f323ed848278048a61681a64c4107d1ace6799
Hao Zhou <hzhou@wpi.edu>
2014-04-06 14:10:15 -0400

    update MD5 generator class

--- src/dds/config/Const.java
+++ src/dds/config/Const.java
@@ -9,11 +9,11 @@ public class Const {
     public static String TIMESTAMP = System.currentTimeMillis() + "";
     
     // MySQL Source Configuration DEV
-    public static String DEV_JDBC_URL = "jdbc:mysql://cs509.wpilife.org:3306/dds_cs509?useUnicode=yes&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
-    public static String DEV_USER = "cs509";
-    public static String DEV_PASSWORD = "CS5092014";
+    public static final String DEV_JDBC_URL = "jdbc:mysql://cs509.wpilife.org:3306/dds_cs509?useUnicode=yes&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+    public static final String DEV_USER = "cs509";
+    public static final String DEV_PASSWORD = "CS5092014";
 
-    // beetl
-    public static String BEETL_ROOT_DIR = "/WEB-INF/beetl";
+    // Security
+    public static final String SALT_FOR_MD5 = "7F4C02829DC3B105E3B88CE948A66947";
 
 }
--- src/dds/kit/SecurityKit.java
+++ src/dds/kit/SecurityKit.java
@@ -1,10 +1,17 @@
 package dds.kit;
 
+import dds.config.Const;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SecurityKit {
 
+	/**
+	 * 
+	 * @param rawStr
+	 * @return md5 encryption of rawStr
+	 * @throws NoSuchAlgorithmException
+	 */
 	public static String getMD5(String rawStr) throws NoSuchAlgorithmException{
 		
 		String original = rawStr;
@@ -16,6 +23,25 @@ public class SecurityKit {
 			sb.append(String.format("%02x", b & 0xff));
 		}
 		return sb.toString();
+	}
+	
+	/**
+	 * 
+	 * @param rawStr
+	 * @return
+	 */
+	public static String encryption(String rawStr){
+		
+		try {
+			return getMD5(getMD5(Const.SALT_FOR_MD5)+getMD5(rawStr));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
+	public static void main(String[] args){
+		System.out.println(SecurityKit.encryption("zhouhao"));
 	}
 	
 }
