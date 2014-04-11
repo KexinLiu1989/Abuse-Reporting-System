418c0cafb8b50b29c8e7addc12d77541c26631d1
Cheng Deng <dc0210@gmail.com>
2014-04-11 00:57:31 -0400

    simple form with validation added

--- WebRoot/app/js/controllers/createReportCtrl.js
+++ WebRoot/app/js/controllers/createReportCtrl.js
@@ -1,6 +1,23 @@
 define([], function() {
 	return ['$scope', '$http', function($scope, $http) {
-        $scope.test = 'test';
+
+        // create angular app
+        var validationApp = angular.module('validationApp', []);
+
+        // create angular controller
+        validationApp.controller('mainController', function($scope) {
+
+            // function to submit the form after all validation has occurred
+            $scope.submitForm = function() {
+
+                // check to make sure the form is completely valid
+                if ($scope.userForm.$valid) {
+                    alert('our form is amazing');
+                }
+
+            };
+
+        });
 
         // because this has happened asynchroneusly we've missed
         // Angular's initial call to $apply after the controller has been loaded
